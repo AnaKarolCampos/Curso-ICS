e831049d9ea2339c1586ca2d80fc0a104b38617d
AnaKarolCampos <camposanakarol@gmail.com>
2024-10-24 16:54:39 -0300

    ex7

--- django/laboratorios/exercicios/urls.py
+++ django/laboratorios/exercicios/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('ex1/', views.ex1, name='ex1'),
     path('ex2/', views.ex2, name='ex2'),
     path('ex3/', views.ex3, name='ex3'),
     path('ex4/', views.ex4, name='ex4'),
     path('ex5/', views.ex5, name='ex5'),
+    path('ex7/', views.ex7, name='ex7'),
+    path('ex8/', views.ex8, name='ex8'),
 ]
--- django/laboratorios/exercicios/views.py
+++ django/laboratorios/exercicios/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+def home(request):
+    return render (request, 'home.html')
+
 def ex1(request):
     return render (request, 'ex1.html')
 
@@ -7,7 +10,7 @@ def ex2(request):
     data = {}
     if request.method == 'POST':
         num1 = request.POST.get('num1')
-        num2 = request.POST.get('num1')
+        num2 = request.POST.get('num2')
         total = int(num1) + int(num2)
         data['total'] = total
     return render (request, 'ex2.html', data)
@@ -40,3 +43,11 @@ def ex5(request):
         frase = f'{palavra1} {palavra2} {palavra3} {palavra4} {palavra5}'
         data['frase'] = frase
     return render (request, 'ex5.html', data)
+
+def ex7(request):
+    data = {}
+    if request.method == 'POST':
+        num1 = request.POST.get('num1')
+        total = int(num1) + int(num1)
+        data['total'] = total
+    return render (request, 'ex7.html', data)
